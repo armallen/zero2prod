f234d2bd717737d56b962dfe06039ef6c3d02538
Arnaud Mallen <mallen.arnaud@gmail.com>
2024-05-15 16:17:02 +0200

    Handle form

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,21 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::{HttpRequest, Responder};
 use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 /*
 > curl http://127.0.0.1:8000
 Hello World!%
@@ -25,9 +37,13 @@ async fn greet(req: HttpRequest) -> impl Responder {
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async // without having to use any proc-macro incantation.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     // No .await here!
     Ok(server)
 }
