fb8db42f57edef4d33046099b3e6eeb15834ef0b
Arnaud Mallen <mallen.arnaud@gmail.com>
2024-05-15 15:42:07 +0200

    Implemented async spawn with random port selection

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{HttpRequest, Responder};
+use std::net::TcpListener;
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+/*
+> curl http://127.0.0.1:8000
+Hello World!%
+> curl http://127.0.0.1:8000/abc
+Hello abc!%
+> curl http://127.0.0.1:8000/abc.txt
+Hello abc.txt!%
+> curl http://127.0.0.1:8000/abc/def
+
+*/
+#[allow(dead_code)]
+async fn greet(req: HttpRequest) -> impl Responder {
+    // Responder is a trait that allows the function to return a variety of types
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async // without having to use any proc-macro incantation.
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
+    // No .await here!
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use zero2prod::run;
+
+use std::net::TcpListener;
+// Tokio allows main to be async
+// This is a rust macro, it operates at token level and implies code is generated at compile time
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    // Bubble up the io::Error if we failed to bind the address // Otherwise call .await on our Server
+    let socket = TcpListener::bind("127.0.0.1:0").unwrap();
+    run(socket)?.await
 }
+
+/*
+> cargo run
+warning: unused manifest key: target.aarch64-apple-darwin.rustflags
+warning: unused manifest key: target.x86_64-apple-darwin.rustflags
+warning: unused manifest key: target.x86_64-pc-windows-gnu.rustflags
+warning: unused manifest key: target.x86_64-pc-windows-msvc.rustflags
+warning: unused manifest key: target.x86_64-unknown-linux-gnu.rustflags
+    Finished dev [unoptimized + debuginfo] target(s) in 0.08s
+     Running `target/debug/zero2prod`
+Error: Os { code: 48, kind: AddrInUse, message: "Address already in use" }
+> lsof -i :8000
+COMMAND     PID   USER   FD   TYPE            DEVICE SIZE/OFF NODE NAME
+zero2prod 49976 mallen    9u  IPv4 0xbfbe3a15ca503c7      0t0  TCP localhost:irdmi (LISTEN)
+
+ ~/workspace/zero2prod/zero2prod | on main !1 ?3 --------------------------------------------------------------------------------------------------------- at 11:26:58
+> kill -9 49976
+
+*/
